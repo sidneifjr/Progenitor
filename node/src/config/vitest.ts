b4be21d700ed51e413e41c98899daba9e01e2271
Sidnei Farias <sidneijr.77@gmail.com>
2024-05-21 14:45:02 -0300

    style(config): fix indentation in vitest-related generated files

--- node/src/config/vitest.ts
+++ node/src/config/vitest.ts
@@ -1,36 +1,33 @@
-export const vitestConfig = `
-  import react from "@vitejs/plugin-react"
-  import { defineConfig } from "vitest/config"
+export const vitestConfig = `import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config"
 
-  export default defineConfig({
-    plugins: [react()],
-    test: {
-      environment: "jsdom",
-    },
-  })
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
+})
 `
 
-export const vitestInitialSpec = `
-  import { expect, test } from 'vitest'
-  import { render, screen } from '@testing-library/react'
-  import Page from '../app/page'
-  
-  test('Page', () => {
-    render(<Page />)
+export const vitestInitialSpec = `import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
 
-    expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
-  })
+test('Page', () => {
+  render(<Page />)
+
+  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+})
 `
 
-export const vitestTestPage = `
-  import Link from 'next/link'
-  
-  export default function Page() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <Link href="/about">About</Link>
-      </div>
-    )
-  }
+export const vitestTestPage = `import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  )
+}
 `
