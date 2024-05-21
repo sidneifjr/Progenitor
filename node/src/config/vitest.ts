16f98f85323e8e669322bdf96c02d3df4936463c
Sidnei Farias <sidneijr.77@gmail.com>
2024-05-21 00:10:00 -0300

    feat(tests): setup vitest tooling

---
+++ node/src/config/vitest.ts
@@ -0,0 +1,36 @@
+export const vitestConfig = `
+  import react from "@vitejs/plugin-react"
+  import { defineConfig } from "vitest/config"
+
+  export default defineConfig({
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+    },
+  })
+`
+
+export const vitestInitialSpec = `
+  import { expect, test } from 'vitest'
+  import { render, screen } from '@testing-library/react'
+  import Page from '../app/page'
+  
+  test('Page', () => {
+    render(<Page />)
+
+    expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+  })
+`
+
+export const vitestTestPage = `
+  import Link from 'next/link'
+  
+  export default function Page() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <Link href="/about">About</Link>
+      </div>
+    )
+  }
+`
--- node/src/procedures/initTooling.ts
+++ node/src/procedures/initTooling.ts
@@ -1,10 +1,15 @@
 import { mkdir, writeFile } from "node:fs/promises"
-import { chdir } from "node:process"
+import { chdir, cwd } from "node:process"
 
 import chalk from "chalk"
 
 import { eslintConfig } from "@/config/eslint"
 import { prettierConfig } from "@/config/prettier"
+import {
+  vitestConfig,
+  vitestInitialSpec,
+  vitestTestPage,
+} from "@/config/vitest"
 import { zodConfig } from "@/config/zod"
 import { runNpmCommand } from "@/utils/run-npm-command"
 
@@ -12,23 +17,36 @@ export async function initTooling() {
   console.log("# Configuring tooling...")
 
   runNpmCommand(
-    "pnpm i prettier prettier-plugin-tailwindcss eslint-plugin-simple-import-sort @rocketseat/eslint-config vitest @testing-library/react zod --save-dev",
+    "pnpm i prettier prettier-plugin-tailwindcss eslint-plugin-simple-import-sort @rocketseat/eslint-config vitest @vitejs/plugin-react jsdom @testing-library/react zod --save-dev",
   )
 
-  writeFile(".prettierrc", prettierConfig)
-  writeFile(".eslintrc.json", eslintConfig)
+  await writeFile(".prettierrc", prettierConfig)
+  await writeFile(".eslintrc.json", eslintConfig)
+  await writeFile("vitest.config.ts", vitestConfig)
 
+  // to do: add a test script to your package.json
   chdir("src/")
-  writeFile("env.ts", zodConfig)
+  await writeFile("env.ts", zodConfig)
 
   const folders = ["actions", "hooks", "modules", "tests", "services"]
   console.log("# Creating folders...")
 
-  folders.map((folder) => {
-    return mkdir(folder)
+  folders.map(async (folder) => {
+    await mkdir(folder)
   })
 
-  console.log()
+  console.log("current working directory:", cwd()) // returns "current working directory D:\Projetos\Personal\progenitor\node\my-app\src"
+
+  // review: cannot find "tests" folder due to incompatibility with promises?
+  chdir("tests")
+  await writeFile("page.test.tsx", vitestInitialSpec)
+
+  // to do: inside 'src' folder, i'll enter the 'app' folder and modify the contents of the page.tsx file.
+  chdir("../app")
+  await mkdir("placeholder")
+
+  chdir("placeholder/")
+  await writeFile("page.tsx", vitestTestPage)
 
   console.log(`
   # ${chalk.bgGreen("Project ready to use!")}
