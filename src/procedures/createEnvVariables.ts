722b50559e3940d2d00721b030b5f0cf612d61af
Sidnei Farias <sidneijr.77@gmail.com>
2024-06-14 13:56:39 -0300

    fix(procedures): remove unnecessary spacing when generating .env-related files

--- src/procedures/createEnvVariables.ts
+++ src/procedures/createEnvVariables.ts
@@ -3,7 +3,7 @@ import { writeFile } from "node:fs/promises"
 // .env files, with mocked values
 export async function createEnvVariables() {
   const envContent = `EXAMPLE_API_KEY=1234
-  NEXT_PUBLIC_API_BASE_URL='https://jsonplaceholder.typicode.com/'`
+NEXT_PUBLIC_API_BASE_URL='https://jsonplaceholder.typicode.com/'`
 
   writeFile(".env", envContent)
   writeFile(".env.example", envContent)
