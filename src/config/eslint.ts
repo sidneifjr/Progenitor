b111c9f1bfbf42f61d79ec99f76a31733f66bf94
Sidnei Farias <sidneijr.77@gmail.com>
2024-12-01 16:18:15 -0300

    fix: update eslint config to include plugins

--- src/config/eslint.ts
+++ src/config/eslint.ts
@@ -1,16 +1,22 @@
 export const eslintConfig = `{
   "extends": [
     "next/core-web-vitals",
-    "@rocketseat/eslint-config/next"
+    "@rocketseat/eslint-config/next",
+    "plugin:sonarjs/recommended-legacy",
+    "plugin:@tanstack/query/recommended"
   ],
+
   "plugins": [
-    "simple-import-sort"
+    "simple-import-sort",
+    "sonarjs"
   ],
+
   "rules": {
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
     "no-console": "warn"
   },
+  
   "parserOptions": {
     "sourceType": "module",
     "ecmaVersion": "latest"
